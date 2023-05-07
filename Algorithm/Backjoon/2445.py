2ce686dca91681eac7ba1776a3c9030de135cabd
TaeDongUm <etd93@naver.com>
2023-05-08 00:11:05 +0900

    5_8 algorithm study

---
+++ Algorithm/Backjoon/2445.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for a in range(0,n):
+    for b in range(0,a+1):
+        print('*', end='')
+    for c in range(0,2*n-2*a-2):
+        print(' ', end='')
+    for d in range(0, a+1):
+        print('*', end='')
+    print('')       
+for e in range(1,n):
+    g = n-e
+    for f in range(0,g):
+        print('*', end='')
+    for h in range(0,e*2):
+        print(' ', end='')
+    for i in range(0, g):
+        print('*', end='')
+    print('')
