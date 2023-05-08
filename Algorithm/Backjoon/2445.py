83ff26d2908837c93703c8b0935665ad7fcbe051
TaeDongUm <etd93@naver.com>
2023-05-08 19:02:08 +0900

    수정

--- Algorithm/Backjoon/2445.py
+++ Algorithm/Backjoon/2445.py
@@ -1,20 +1,20 @@
 n = int(input())
 
 for a in range(0,n):
-    for b in range(0,a+1):
+    for c in range(0,a+1):
         print('*', end='')
     for c in range(0,2*n-2*a-2):
         print(' ', end='')
-    for d in range(0, a+1):
+    for c in range(0, a+1):
         print('*', end='')
     print('')       
-for e in range(1,n):
-    g = n-e
-    for f in range(0,g):
+for b in range(1,n):
+    d = n-b
+    for c in range(0,d):
         print('*', end='')
-    for h in range(0,e*2):
+    for c in range(0,b*2):
         print(' ', end='')
-    for i in range(0, g):
+    for c in range(0, d):
         print('*', end='')
     print('')
 # commit test
